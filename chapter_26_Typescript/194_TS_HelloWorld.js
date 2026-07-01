cd21217292a2684cbf28706b024ec542835cc93e
pathansaif347 <pathansaif347@gmail.com>
2026-07-01 15:33:02 +0530

    Add chapters 21-27: Classes, Encapsulation, Inheritance, OOP Interview Questions, and TypeScript

---
+++ chapter_26_Typescript/194_TS_HelloWorld.js
@@ -0,0 +1,5 @@
+"use strict";
+let testName = "Hello World";
+function greate(a, b) {
+    return a + b;
+}
---
+++ chapter_26_Typescript/194_TS_HelloWorld.ts
@@ -0,0 +1,6 @@
+
+let testName :string= "Hello World";
+
+function greate (a: number, b:number) :number{
+    return a+b;
+}
